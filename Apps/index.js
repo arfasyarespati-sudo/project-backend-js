87f90fa1c6beb6671330c2076b4b506395a5b679
Muhammad Arfasya <arfasyarespati@gmail.com>
2026-08-03 17:38:21 +0700

    use express json

--- Apps/index.js
+++ Apps/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const app = express()
+const app = express();
+
 const hostname = '127.0.0.1';
 const port = 3000;
+
+app.use(express.json());
 
 const quotes = [
   {id: 1, text: "test1", author: "Anonymus"},
   {id: 2, text: "test2", author: "Yaok"},
   {id: 3, text: "test3", author: "Enkripsi"}
 ];
-
-
 
 app.get('/', (req, res) => {
   res.send('hello from express');
