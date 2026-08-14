d880e19a955bf323f5a7b4e464cb24fa9b3c167a
arfasyarespati-sudo <arfasyarespati@gmail.com>
2026-08-14 17:26:34 +0700

    server initializing

---
+++ Apps/myHttp.js
@@ -0,0 +1,24 @@
+/*const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    res.end('Hello World!\n');
+});
+
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}/`)
+});*/
+
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, {'Content-type': 'text'});
+    res.end('Hello Sekai\n');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}/`)
+})
