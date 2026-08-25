dc2449a1b2b5d41f55fa2474ebda8a56ccabf806
arfasyarespati-sudo <arfasyarespati@gmail.com>
2026-08-25 21:28:32 +0700

    middleware safety: GET GET

--- Apps/server.js
+++ Apps/server.js
@@ -6,6 +6,11 @@ const port = 3000;
 
 app.use(cors())
 
+app.use((req, res, next) => {
+    console.log(req.method, req.path)
+    next()
+})
+
 app.use(express.json())
 
 app.use('/products', productsRouter)
